7a8b7074a0bc922e926840c1e409dbb42ab2a01f
kevz <vzcodesthings@gmail.com>
2019-06-24 06:22:25 -0700

    Using handlers and swapped out env variables

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const fetch = require("node-fetch");
 
-const s3 = new AWS.S3({
-    accessKeyId: '[]',
-    secretAccessKey: '[]
-})
-
 const imagesUrl = 'https://loremflickr.com/320/240/eggs';
 
-const downloadFile = (async (url) => {
+const s3 = new AWS.S3({
+    accessKeyId: process.env.s3AccessKey,
+    secretAccessKey: process.env.s3secretKey
+})
+
+exports.handler = async () => {
+    const downloadFile = (async (url) => {
     const res = await fetch(url);
     const fileStream = fs.createWriteStream('tempImage.jpg');
     await new Promise((resolve, reject) => {
@@ -22,22 +23,39 @@ const downloadFile = (async (url) => {
             console.log('Image downloaded from API and created');
         });
     });
-});
+    });
 
-const uploadFile = () => {
-    fs.readFile('./tempImage.jpg', function(err, imageFile) {
-        let params = {
-            Bucket: 'bas-c',
-            Key: 'bg.jpg',
-            Body: imageFile,
-            ContentType: 'binary'
-        }
-        s3.upload(params, function(err,data) {
-            if (data) {
-                console.log('Upload success')
+    const uploadFile = () => {
+        fs.readFile('./tempImage.jpg', function(err, imageFile) {
+            let params = {
+                Bucket: 'bas-c',
+                Key: 'bg.jpg',
+                Body: imageFile,
+                ContentType: 'binary'
             }
+            s3.upload(params, function(err,data) {
+                if (data) {
+                    console.log('Upload success')
+                }
+            })
         })
-    })
+    };
+
+    const response = () => {
+        downloadFile(imagesUrl).then(() => uploadFile());
+    };
+
+    response();
 };
 
-downloadFile(imagesUrl).then(() => uploadFile());
+exports.handler = async function (event, context) {
+    console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
+    console.log("EVENT\n" + JSON.stringify(event, null, 2))
+    return context.logStreamName
+};
+
+
+
+
+
+
