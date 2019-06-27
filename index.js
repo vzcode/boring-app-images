5df546047d70a8c54e2cc06b344719509dc71607
kevz <vzcodesthings@gmail.com>
2019-06-26 20:19:05 -0700

    Initial

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
 const AWS = require('aws-sdk');
-const fs = require('fs');
-const fetch = require("node-fetch");
+const request = require('request-promise')
+
 
 const s3 = new AWS.S3({
     accessKeyId: '[]',
-    secretAccessKey: '[]
+    secretAccessKey: '[]'
 })
 
 const imagesUrl = 'https://loremflickr.com/320/240/eggs';
 
-const downloadFile = (async (url) => {
-    const res = await fetch(url);
-    const fileStream = fs.createWriteStream('tempImage.jpg');
-    await new Promise((resolve, reject) => {
-        res.body.pipe(fileStream);
-        res.body.on("error", (err) => {
-            reject(err);
-        });
-        fileStream.on("finish", function () {
-            resolve();
-            console.log('Image downloaded from API and created');
-        });
-    });
-});
-
-const uploadFile = () => {
-    fs.readFile('./tempImage.jpg', function(err, imageFile) {
-        let params = {
-            Bucket: 'bas-c',
-            Key: 'bg.jpg',
-            Body: imageFile,
-            ContentType: 'binary'
-        }
-        s3.upload(params, function(err,data) {
-            if (data) {
-                console.log('Upload success')
-            }
-        })
-    })
+const options = {
+    uri: imagesUrl,
+    encoding: null
 };
 
-downloadFile(imagesUrl).then(() => uploadFile());
+async function load() {
+
+    const body = await request(options)
+
+    const uploadResult = await s3.upload({
+        Bucket: 'bas-c',
+        Key: 'bg.jpg',
+        Body: body,
+    }).promise()
+
+}
+
